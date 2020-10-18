bdb1d25a66d6c526b6be6696c8e6c85108d007f9
totallynotbakadestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2020-10-18 19:54:11 +0300

    Add Vue

--- app.js
+++ app.js
@@ -1,60 +1,29 @@
-require('dotenv-flow').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
-const mongoose = require('mongoose');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require("dotenv-flow").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var sassMiddleware = require("node-sass-middleware");
+var history = require("connect-history-api-fallback");
+const mongoose = require("mongoose");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(history());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// set up database connection
 const mongoURI = process.env.mongoURI;
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 
-db.on('connected', console.log.bind(console, 'Established connection with MongoDB'));
-
-
+db.on("connected",
+    console.log.bind(console, "Established connection with MongoDB")
+);
 
 module.exports = app;
